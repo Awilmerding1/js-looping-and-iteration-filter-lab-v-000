7d70c8133c49bd1e7b4b665fca4d2e56f4459fbb
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-27 19:16:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 
 function findMatching(drivers, name) {
-  const d = drivers.filter(function(driver) {return driver === name && driver === name.capitali});
+  const d = drivers.filter(function(driver) {return driver.toUpperCase() === name.toUpperCase()});
   return d;
 }
