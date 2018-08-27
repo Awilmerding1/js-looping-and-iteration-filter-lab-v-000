b0ec6ee336848aa661dce79a876f5b193120c0ac
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-27 19:04:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file
 
 function findMatching(drivers, name) {
-  drivers.filter(function(driver, name) {return driver[name] === name})
+  drivers.filter(function(driver, name) {return driver[name] === name});
 }
