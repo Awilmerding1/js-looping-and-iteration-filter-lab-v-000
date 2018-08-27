2015def82c4d59e02cf2a8c910a741edddc9493f
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-27 19:21:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function fuzzyMatch(drivers, letters) {
 }
 
 function matchName(drivers, string) {
-  const d = drivers.filter(function(driver) {return driver[name] })
+  const d = drivers.filter(function(driver) {return driver[name] === string})
 }
