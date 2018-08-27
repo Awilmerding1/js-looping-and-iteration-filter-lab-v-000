c99a1ac96700324346cb5c3b31c0a6110a087444
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-27 19:09:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 
 function findMatching(drivers, name) {
-  const d = drivers.filter(function(driver) {return driver === name && driver === name.toLowerCase});
+  const d = drivers.filter(function(driver) {return driver === name && driver.toLowerCase === name});
   return d;
 }
